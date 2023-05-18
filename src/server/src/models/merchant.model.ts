cf3b9a89e53d8c7313e542f585329cb379fe8376
Varnit <varnits02@gmail.com>
2023-05-18 14:45:48 -0500

    merchant and transaction type models

---
+++ src/server/src/models/merchant.model.ts
@@ -0,0 +1,10 @@
+/**
+ * Merchant model
+ */
+export interface Merchant {
+  // serial id of the merchant
+  id?: number;
+
+  // merchant name
+  name?: string;
+}
---
+++ src/server/src/models/transaction_type.model.ts
@@ -0,0 +1,10 @@
+/**
+ * Transaction Type model
+ */
+export interface TransactionType {
+  // serial id of the transaction type
+  id?: number;
+
+  // transaction type
+  type?: string;
+}
