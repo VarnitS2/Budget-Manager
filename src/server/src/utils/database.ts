0dd59694e1000cac96a9a6fa434c5e603793101d
Varnit <varnits02@gmail.com>
2023-05-23 17:51:35 -0500

    update db initialization to match new db schema

--- src/server/src/utils/database.ts
+++ src/server/src/utils/database.ts
@@ -26,7 +26,7 @@ const dbPromise = (async (): Promise<Database> => {
       CREATE TABLE IF NOT EXISTS merchants (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL UNIQUE
-        categoryID INTEGER REFERENCES categories(id)
+        categoryID INTEGER REFERENCES categories(id) NULL
       );
     `);
     logger.success(`merchants table check passed`);
@@ -35,7 +35,6 @@ const dbPromise = (async (): Promise<Database> => {
       CREATE TABLE IF NOT EXISTS transactions (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         merchantID INTEGER REFERENCES merchants(id),
-        categoryID INTEGER REFERENCES categories(id),
         amount DECIMAL(5,2) NOT NULL,
         date DATE NOT NULL
       );
