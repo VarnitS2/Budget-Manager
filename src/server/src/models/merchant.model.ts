e2e3eb2e28e991e8ee9de59d0c729618d1eab478
Varnit <varnits02@gmail.com>
2023-05-24 11:30:45 -0500

    add MerchantRequest model

--- src/server/src/models/merchant.model.ts
+++ src/server/src/models/merchant.model.ts
@@ -11,3 +11,20 @@ export interface Merchant {
   // serial id of the category of the merchant
   categoryID?: number;
 }
+
+/**
+ * Merchant Request model
+ */
+export interface MerchantRequest {
+  // serial id of the merchant
+  id?: number;
+
+  // name of the merchant
+  name?: string;
+
+  // name of the category of the merchant
+  categoryName?: string;
+
+  // multiplier of the category of the merchant
+  multiplier?: number;
+}
