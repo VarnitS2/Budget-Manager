e53e31f7a679b9b46b60534763431df9eb6372c8
Varnit <varnits02@gmail.com>
2023-06-14 16:40:52 -0500

    Add mantine provider and dark theme

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <MantineProvider theme={{ colorScheme: "dark" }} withGlobalStyles withNormalizeCSS>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MantineProvider>
   </React.StrictMode>
 );
 
