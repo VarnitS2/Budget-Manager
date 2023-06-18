bacf8bc138ce5d8f567f00819c259818da616ede
Varnit <varnits02@gmail.com>
2023-06-18 16:50:49 -0500

    add notifications

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { MantineProvider } from "@mantine/core";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from '@mantine/notifications';
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <MantineProvider theme={{ colorScheme: "dark" }} withGlobalStyles withNormalizeCSS>
+      <Notifications />
       <BrowserRouter>
         <App />
       </BrowserRouter>
